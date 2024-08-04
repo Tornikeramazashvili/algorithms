ab3b01f8dbe3eaa5fe20a073c2f6cea6016cdc9b
tornikeramazashvili <tornikeramazashvili@tornikeramazashvilis-MacBook-Air.local>
2024-08-04 13:25:48 +0400

    initial commit

---
+++ 1. Big O Notation/Introduction.js
@@ -0,0 +1,69 @@
+/* If you have multiple implementations of the same function,
+how would you determine which one is the best? 🤔
+
+That's where Big O notation comes in 🚀
+
+Big O helps us measure the efficiency of our code, and focuses 
+on how the algorithm's runtime grows relative to the size of the input 📈
+
+For example if we want to write a function that 
+calculates the sum of a number 🔢
+*/
+
+// example 1: 
+
+function addUpTo1(n) {
+  let total = 0;
+  for (let i = 0; i <= n; i++) {
+    total = total + i;
+  }
+  return total;
+}
+
+console.log(addUpTo1(5)); // output: 15
+
+// example 2: 
+
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+
+console.log(addUpTo2(5)); // output: 15
+
+/* Outputs are the same, so which one is the best and what does BETTER mean? 🤔
+
+Faster? ⚡
+Less memory-intensive? 💪🏻   
+More readable? 🔎
+
+You might say that readability is important, well it is, but actually speed 
+is much more important in terms of algorithms 🛠️
+
+We can use performance.now() function that can measure how long takes our code to run 🏃
+
+for example:
+*/
+
+let startingPoint = performance.now();
+addUpTo1(1000000000);
+let endingPoint = performance.now();
+console.log(`Time elapsed: ${(endingPoint - startingPoint) / 1000} seconds`);
+
+let startingPoint2 = performance.now();
+addUpTo2(1000000000);
+let endingPoint2 = performance.now();
+console.log(`Time elapsed: ${(endingPoint2 - startingPoint2) / 1000} seconds`);
+
+/* We can clearly see that addUpTo2 is much faster than addUpTo1. Why does this happen? 🤔 
+
+addUpTo1 uses 5n + 2 operations because of the FOR LOOP (n addition, n assignment, n comparison, n addition, n assignment, 1 assignment, 1 assignment).
+as the input n GROWS larger, the time required to complete the function GROWS linearly, Therefore GROWS INPUT = GROWS TIME 📈
+so the time complexity will be O(n).
+
+addUpTo2 uses 3 simple operations (multiplication, addition, division), regardless of the size of n.
+as the input n GROWS LAGER, the execution time REMAINS constant, Therefore GROWS INPUT = DOES NOT GROW TIME 📉
+so the time complexity is O(1).
+
+Finally we can say that addUpTo1 requires more iterations because of the loop n + 1 🌀,
+while addUpTo2 remains fast even for very large values of n because it performs a fixed number of operations 🧮.
+*/
---
+++ README.md
@@ -0,0 +1,13 @@
+# JavaScript Algorithms and Data Structures
+
+## Introduction
+
+I've decided to document various algorithms and data structures in JavaScript to illustrate their impact on performance, scalability, and overall code quality.
+
+Feel free to learn from, share, and use this information in your own projects or however you see fit.
+
+## Folders and Files Sequence
+
+**1. Big O Notation**: This folder contains resources related to Big O notation, including explanations and examples.
+
+- **Introduction.js**: This file provides an introduction to Big O notation and demonstrates the difference between \(O(n)\) and \(O(1)\) time complexities using two implementations of a function that calculates the sum of integers.
